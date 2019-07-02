a916020d8ee4d9dccc76b54c3b72c4bac43e8b1e
Anders Green <github@louie.se>
2019-07-02 11:29:48 +0200

    Fixar hannes fulkod :P

--- bot.py
+++ bot.py
@@ -9,36 +9,48 @@ import os
 
 from svultron import svultron
 
+
 # Subclass fbchat.Client and override required methods
 class EchoBot(Client):
-    def send_reply(reply_text):
-        log.info("{}: reply text: '{}'".format(message_object.text[1:], reply_text))
-        client.send(Message(text=reply_text), thread_id=thread_id, thread_type=thread_type)
-    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        self.markAsDelivered(thread_id, message_object.uid)
-        self.markAsRead(thread_id)
+    def send_reply(self, command, reply_text, thread_id, thread_type):
+        log.info("{}: reply text: '{}'".format(command, reply_text))
+        client.send(
+            Message(text=reply_text),
+            thread_id=thread_id,
+            thread_type=thread_type
+        )
 
-        if author_id != self.uid and message_object.text[0] == '!':
-            # load commands
-            with open('commands.json') as command_config:
-                json_commands = command_config.read()
-                commands = json.loads(json_commands)
+    def onMessage(self, uid, msg, tid, ttype, **kwargs):
+        self.markAsDelivered(tid, msg.uid)
+        self.markAsRead(tid)
 
-                if message_object.text[1:] == 'svultron':
-                    self.send_reply(svultron())
-                elif message_object.text[1:] in commands:
-                    self.send_reply(commands[message_object.text[1:]])
-                else:
-                    log.info("Ignoring unknown command {}".format(message_object.text[1:]))
+        if uid != self.uid and msg.text[0] == '!':
+            if msg.text[1:] == 'svultron':
+                self.send_reply(msg.text[1:], svultron(), tid, ttype)
+            else:
+                # load commands
+                with open('commands.json') as command_config:
+                    json_commands = command_config.read()
+                    commands = json.loads(json_commands)
+
+                    if msg.text[1:] in commands:
+                        self.send_reply(
+                            msg.text[1:],
+                            commands[msg.text[1:]],
+                            tid,
+                            ttype
+                        )
+                    else:
+                        log.info("Unknown '{}'".format(msg.text[1:]))
 
 
 email = os.environ.get('EMAIL')
 password = os.environ.get('PASSWD')
 
 if email is None or password is None:
-    print "Please set environment variables to be able to login."
-    print "\tEMAIL   - The email of the user."
-    print "\tPASSWD  - The password of the user."
+    print("Please set environment variables to be able to login.")
+    print("\tEMAIL   - The email of the user.")
+    print("\tPASSWD  - The password of the user.")
 else:
     client = EchoBot(email, password)
     client.listen()
