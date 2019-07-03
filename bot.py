a95aed39937c9f07a2ff78ef7b746465b10b5be8
Anders Green <github@louie.se>
2019-07-03 09:25:50 +0200

    Buggfixar

--- bot.py
+++ bot.py
@@ -13,39 +13,58 @@ from svultron import svultron
 
 # Subclass fbchat.Client and override required methods
 class EchoBot(Client):
-    def send_reply(self, command, reply_text, thread_id, thread_type):
-        log.info("{}: reply text: '{}'".format(command, reply_text))
+    def send_reply(self, author_id, command, reply_text,
+                   thread_id, thread_type):
+        author = client.fetchUserInfo(author_id).get(author_id, "<Unknown>")
+        log.warning("{}: '{}' -> '{}'".format(
+            author.name,
+            command,
+            reply_text.replace("\n", " | ")
+        ))
         client.send(
             Message(text=reply_text),
             thread_id=thread_id,
             thread_type=thread_type
         )
 
-    def onMessage(self, uid, msg, tid, ttype, **kwargs):
-        self.markAsDelivered(tid, msg.uid)
-        self.markAsRead(tid)
+    def onMessage(self, author_id, message_object,
+                  thread_id, thread_type, **kwargs):
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
 
-        if uid != self.uid and msg.text[0] == '!':
-            if msg.text[1:] == 'svultron':
-                self.send_reply(msg.text[1:], svultron(), tid, ttype)
+        if author_id != self.uid and message_object.text[0] == '!':
+            # Handle !svultron as a special case. It calls an external
+            # module that generates a fake quote based on keywords.
+            if message_object.text[1:] == 'svultron':
+                self.send_reply(author_id, message_object.text[1:],
+                                svultron(), thread_id, thread_type)
             else:
-                # load commands
+                # re-load commands
+                # FIXME: should be a way not needing to reload the
+                #        commands.json file every time. maybe add a
+                #        command-command like !refresh
                 with open('commands.json') as command_config:
                     json_commands = command_config.read()
                     commands = json.loads(json_commands)
 
-                    if msg.text[1:] in commands:
-                        response = commands[msg.text[1:]]
+                    # If the command exists return the value matching
+                    # the key. If the value is a list we return a random
+                    # item in that list.
+                    if message_object.text[1:] in commands:
+                        response = commands[message_object.text[1:]]
                         if isinstance(response, list):
-                            response = random.choice(commands[msg.text[1:]])
+                            response = random.choice(
+                                commands[message_object.text[1:]])
                         self.send_reply(
-                            msg.text[1:],
+                            author_id,
+                            message_object.text[1:],
                             response,
-                            tid,
-                            ttype
+                            thread_id,
+                            thread_type
                         )
                     else:
-                        log.info("Unknown '{}'".format(msg.text[1:]))
+                        log.info("Unknown '{}'".format(
+                            message_object.text[1:]))
 
 
 email = os.environ.get('EMAIL')
@@ -56,5 +75,5 @@ if email is None or password is None:
     print("\tEMAIL   - The email of the user.")
     print("\tPASSWD  - The password of the user.")
 else:
-    client = EchoBot(email, password)
+    client = EchoBot(email, password, logging_level=30)
     client.listen()
